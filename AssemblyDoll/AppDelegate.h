f62a53cd5f9872fb4ab025119f6da3d1e333b3ef
eve0770 <calvin.kl1026@gmail.com>
2017-03-31 10:30:04 +0800

    Initial Commit

---
+++ AssemblyDoll/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AssemblyDoll
+//
+//  Created by NEXTAcademy on 3/31/17.
+//  Copyright © 2017 ArchieApp. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ AssemblyDoll/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AssemblyDoll
+//
+//  Created by NEXTAcademy on 3/31/17.
+//  Copyright © 2017 ArchieApp. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
